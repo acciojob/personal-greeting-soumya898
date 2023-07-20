cfbebee35552d0e9db91173ed3ed7b7aa9d691aa
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-07-20 15:27:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => 
-  const [name.setName]=useState("");
+  const [name,setName]=useState("");
 
   return (
     <div>
